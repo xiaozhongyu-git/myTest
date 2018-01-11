9dcdfbc39fcc6c509fe1ecfaa2653f749bbbbace
xiaozhongyu-git <34338963+xiaozhongyu-git@users.noreply.github.com>
2018-01-11 14:39:26 +0800

    Update HelloWorld.java
    修改创建的java文件。

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
   public static void main(String[] args){
-      System.out.println("Hello World!");
+      System.out.println("第一次提交：Hello World!");
+      System.out.println("第二次提交：Hello World!");
   }
 }
