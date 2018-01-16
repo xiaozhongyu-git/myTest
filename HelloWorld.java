48b6bf24ff9cae4f90be04c7a91cac0c80548c4c
1823658854@qq.com <1826260883@qq.com>
2018-01-16 15:56:29 +0800

    第二次通过git提交到仓库

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
   public static void main(String[] args){
       System.out.println("第一次提交：Hello World!");
       System.out.println("第二次提交：Hello World!");
+      System.out.println("通过git操纵远程仓库");
   }
 }
